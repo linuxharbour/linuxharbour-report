55220d67af25d5a4df7c3a5366eb11032b848103
Sammy Fung <sammy@sammy.hk>
2014-04-29 15:59:29 +0800

    added subtitles and networking information

--- bin/linuxharbour-report.sh
+++ bin/linuxharbour-report.sh
@@ -8,20 +8,43 @@ echo "===================================="
 echo " "
 
 hostname=`hostname -f`
+repdate=`date`
 echo System: ${hostname}
+echo Report Time: ${repdate}
 echo " "
 
 ### Number of Cores and CPU Model
 echo "Processor"
-echo "========="
+echo "---------"
 grep "^model name" /proc/cpuinfo | sort | uniq -c
 echo " "
 
 ### System Uptime
+echo "System Uptime and Current Loading"
+echo "---------------------------------"
 uptime
+echo " "
 
 ### Memory Size and Usage
+echo "Memory Size and Usage"
+echo "---------------------"
 free -h
+echo " "
 
 ### Storage Usage
+echo "Storage Usage"
+echo "-------------"
 df -h
+echo " "
+
+### Network Interfaces
+echo "Network Interfaces"
+echo "------------------"
+/sbin/ifconfig
+echo " "
+
+### Network Route
+echo "Network Routing"
+echo "---------------"
+/sbin/route -n
+echo " "
